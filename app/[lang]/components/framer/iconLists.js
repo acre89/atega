d0d9da3e9b9a91dddb75df292a7f57331641f615
acre89 <ajlcresci@gmail.com>
2024-07-21 11:46:15 +0200

    tel3

--- app/[lang]/components/framer/iconLists.js
+++ app/[lang]/components/framer/iconLists.js
@@ -6,7 +6,33 @@ import { useRef } from "react";
 export default function IconListAnimate({ children, delay }) {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
-
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 768;
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    // subscribe to window resize event "onComponentDidMount"
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      // unsubscribe "onComponentDestroy"
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+  if (width > breakpoint) {
+    return (
+      <ul
+      className="flex flex-col items-center gap-3 w-28"
+      ref={ref}
+      style={{
+        transform: isInView ? "none" : "translateY(20px)",
+        opacity: isInView ? 1 : 0,
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
+        transitionDelay: delay,
+      }}
+    >
+      {children}
+    </ul>
+    );
+  }
   return (
     <ul
       className="flex flex-col items-center gap-3 w-28"
@@ -21,4 +47,5 @@ export default function IconListAnimate({ children, delay }) {
       {children}
     </ul>
   );
+ 
 }
--- app/[lang]/components/framer/list.js
+++ app/[lang]/components/framer/list.js
@@ -1,12 +1,38 @@
 "use client";
 
 import { useInView } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default function ListAnimate({ children, delay }) {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
-
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 768;
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    // subscribe to window resize event "onComponentDidMount"
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      // unsubscribe "onComponentDestroy"
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+  if (width > breakpoint) {
+    return (
+      <li
+      className="list-item w-full"
+      ref={ref}
+      style={{
+        transform: isInView ? "none" : "translateY(20px)",
+        opacity: isInView ? 1 : 0,
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
+        transitionDelay: delay,
+      }}
+    >
+      {children}
+    </li>
+    );
+  }
   return (
     <li
       className="list-item w-full"
@@ -15,7 +41,7 @@ export default function ListAnimate({ children, delay }) {
         transform: isInView ? "none" : "translateY(20px)",
         opacity: isInView ? 1 : 0,
         transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
-        transitionDelay: delay,
+
       }}
     >
       {children}
